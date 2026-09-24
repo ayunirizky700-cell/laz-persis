5919cc2a6a4596cc7b5df6ed74aaed3f7279b68b
silvia <silviaalmaida9@gmail.com>
2026-09-24 16:34:01 +0700

    Dashboard: kartu statistik

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,7 +28,6 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'nama' => 'required|string|max:100',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:6',
             'role_id' => 'required|exists:roles,id',
@@ -37,7 +36,6 @@ class UserController extends Controller
 
         User::create([
             'name' => $request->name,
-            'nama' => $request->nama,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role_id' => $request->role_id,
@@ -59,7 +57,6 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'nama' => 'required|string|max:100',
             'email' => 'required|email|unique:users,email,' . $user->id,
             'role_id' => 'required|exists:roles,id',
             'status' => 'required|in:aktif,nonaktif',
@@ -67,7 +64,6 @@ class UserController extends Controller
 
         $data = [
             'name' => $request->name,
-            'nama' => $request->nama,
             'email' => $request->email,
             'role_id' => $request->role_id,
             'status' => $request->status,
