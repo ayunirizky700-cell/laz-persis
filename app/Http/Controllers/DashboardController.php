d2dffe77cd8959b5c300276fb40d199c5632e1a7
Ayuni Rizky <ayunirizky@gmail.com>
2026-09-25 10:09:11 +0700

    Fix: Dashboard + import Model Amil

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Muzakki;
+use App\Models\Mustahik;
+use App\Models\Program;
+use App\Models\Penerimaan;
+use App\Models\Penyaluran;
 use App\Models\User;
-use App\Models\Role;
-use Illuminate\Http\Request;
+use App\Models\Amil;             
 
-class DashboardController extends Controller
+  class DashboardController extends Controller
 {
     public function index()
     {
